309637b197c9c1cebf6dd1ad3b3ed958fba9a85c
hoanguyennfq <dieuhoa.nguyen@nfq.com>
2023-05-19 18:01:26 +0700

    DEVOPS-2958

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  alias   = "second"
+  region = ap-southeast-1b
+  # aws cli user credentials
+  # user should have the administration policy or policy as per the lpp principle
+  access_key = "AKIA6xxxxxxxxxxxxxxx"
+  secret_key = "TpxiKRxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
