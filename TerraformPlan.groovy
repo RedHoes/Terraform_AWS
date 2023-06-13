e2304cc26a949d2904e551317d326e3e185beed6
longdinger <longnguyen@VNDANLAP-0003.localdomain>
2023-06-13 19:35:07 +0700

    DEVOPS-2968: lab 1

--- TerraformPlan.groovy
+++ TerraformPlan.groovy
@@ -4,26 +4,45 @@ pipeline {
         string(name: 'gitBranch', defaultValue: 'DEVOPS-2963-Lab1', description: 'Git Branch')
     }
 
-    triggers {
-        GenericTrigger(
-            genericVariables: [
-                causeString: 'Triggered on $gitBranch',
-                token: 'long'
-            ]
-        )
-    }
+  triggers {
+    GenericTrigger(
+      genericVariables: [
+        [key: 'gitBranch', value: '$.ref'],
+      ],
+
+      causeString: 'Triggered on $gitBranch',
+
+      token: "long",
+
+      printContributedVariables: true,
+      printPostContent: true,
+
+      silentResponse: false,
+
+      regexpFilterText: '$gitBranch',
+      regexpFilterExpression: '^refs/heads/DEVOPS-2963-Lab1'
+    )
+  }
 
     stages {
         stage('Terraform plan') {
+            environment {
+                lab1 = credentials('Long-Private-Key')
+                lab1pub = credentials('	Long-public-Key')
+            }
             steps {
-                sh '''
-                    cp -r files lab1
-                    cd lab1
-                    terraform init
-                    terraform validate
-                    terraform plan
-                '''
+                dir("lab1") {
+                    sh '''
+                        ls -la
+                        cp ${lab1} files/lab1
+                        cp ${lab1pub} files/lab1.pub
+                        terraform init
+                        terraform validate
+                        terraform plan
+                    '''
+                }
             }
         }
     }
 }
+
