d694f60f3a608abd6c2c099e4a9326a5bd516319
longdinger <longnguyen@VNDANLAP-0003.localdomain>
2023-06-13 20:34:55 +0700

    DEVOPS-2968: lab 1 Jenkins

--- TerraformPlan.groovy
+++ TerraformPlan.groovy
@@ -23,26 +23,66 @@ pipeline {
       regexpFilterExpression: '^refs/heads/DEVOPS-2963-Lab1'
     )
   }
+    // stages {
+    //     stage('Terraform plan') {
+    //         environment {
+    //             lab1 = credentials('Long-Private-Key')
+    //             lab1pub = credentials('	Long-public-Key')
+    //         }
+    //         steps {
+    //             dir("lab1") {
+    //                 sh '''
+    //                     cp ${lab1} files/lab1
+    //                     cp ${lab1pub} files/lab1.pub
+    //                     terraform init
+    //                     terraform validate
+    //                     terraform plan
+    //                 '''
+    //             }
+    //         }
+    //     }
+    // }
 
     stages {
-        stage('Terraform plan') {
-            environment {
-                lab1 = credentials('Long-Private-Key')
-                lab1pub = credentials('	Long-public-Key')
-            }
+        stage('Process Webhook') {
             steps {
-                dir("lab1") {
-                    sh '''
-                        cp ${lab1} files/lab1
-                        cp ${lab1pub} files/lab1.pub
-                        terraform init
-                        terraform validate
-                        terraform plan
-                    '''
+                script {
+                    // Check if it's a pull request or merge request
+                    if (env.gitBranch.startsWith('refs/pull/')) {
+                        // Execute pull request function
+                        pullRequestFunction()
+                    } else if (env.gitBranch.startsWith('refs/heads/')) {
+                        // Execute merge request function
+                        mergeRequestFunction()
+                    }
                 }
             }
         }
     }
-    // Long .
 }
 
+def pullRequestFunction() {
+    stage('Pull Request Function') {
+        steps {
+          sh '''
+            cp ${lab1} files/lab1
+            cp ${lab1pub} files/lab1.pub
+            terraform init
+            terraform validate
+            terraform plan
+          '''
+        }
+    }
+}
+
+def mergeRequestFunction() {
+    stage('Merge Request Function') {
+        steps {
+          sh '''
+            terraform init
+            terraform validate
+            terraform apply
+          '''
+        }
+    }
+}
