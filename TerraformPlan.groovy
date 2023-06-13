ac0ba38e9337b438d11dad6fe7a8b1578ccbbc98
longdinger <longnguyen@VNDANLAP-0003.localdomain>
2023-06-14 00:16:58 +0700

    DEVOPS-2968: lab 1 Jenkins

--- TerraformPlan.groovy
+++ TerraformPlan.groovy
@@ -34,7 +34,7 @@ pipeline {
                 dir("lab1") {
                     sh '''
                         ls -la
-                        rmdir files
+                        rmdir -rf files
                         mkdir files
                         cp ${lab1} files/lab1
                         cp ${lab1pub} files/lab1.pub
