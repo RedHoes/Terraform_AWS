dcec9a90dccfd4e982396be2ea38ffee6f8fe7a2
longdinger <longnguyen@VNDANLAP-0003.localdomain>
2023-06-14 09:48:56 +0700

    DEVOPS-2968: lab 1 Jenkins

--- TerraformPlan.groovy
+++ TerraformPlan.groovy
@@ -1,19 +1,21 @@
 /* groovylint-disable LineLength */
+/* groovylint-disable-next-line CompileStatic */
 pipeline {
     agent any
     parameters {
         string(name: 'gitBranch', defaultValue: 'DEVOPS-2963-Lab1', description: 'Git Branch')
     }
 
-  triggers {
-    GenericTrigger(
+    triggers {
+        GenericTrigger(
       genericVariables: [
+        /* groovylint-disable-next-line DuplicateStringLiteral */
         [key: 'gitBranch', value: '$.ref'],
       ],
 
       causeString: 'Triggered on $gitBranch',
 
-      token: "long",
+      token: 'long',
 
       printContributedVariables: true,
       printPostContent: true,
@@ -23,7 +25,7 @@ pipeline {
       regexpFilterText: '$gitBranch',
       regexpFilterExpression: '^refs/heads/DEVOPS-2963-Lab1'
     )
-  }
+    }
 
     stages {
         stage('Terraform plan') {
@@ -32,12 +34,13 @@ pipeline {
                 lab1pub = credentials('Long-public-Key')
             }
             steps {
-                dir("lab1") {
-                  // sudo rm -rf files
+                dir('lab1') {
+                    // sudo rm -rf files
+                    /* groovylint-disable-next-line GStringExpressionWithinString */
                     sh '''
                       sudo rm -rf files
                       mkdir files
-                      ls -la 
+                      ls -la
                       cp ${lab1} files/lab1
                       cp ${lab1pub} files/lab1.pub
                       terraform init
@@ -50,12 +53,12 @@ pipeline {
     }
 
     environment {
-      trainee_webhook_token = credentials('trainee-slack-token')
+        trainee_webhook_token = credentials('trainee-slack-token')
     }
 
     post {
-        always { 
-            slackSend(channel: 'alerts', baseUrl: 'https://nfq-international.slack.com', teamDomain: 'nfq-international' ,token: env.trainee_webhook_token, message: "Hello World")
+        always {
+            slackSend channel: 'alerts'
         }
     } 
 }
