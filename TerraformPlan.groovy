558075a35cce63abdf950c2d4eab35b9f6cf7866
longdinger <longnguyen@VNDANLAP-0003.localdomain>
2023-06-14 09:59:58 +0700

    DEVOPS-2968: lab 1 Jenkins

--- TerraformPlan.groovy
+++ TerraformPlan.groovy
@@ -58,7 +58,7 @@ pipeline {
 
     post {
         always {
-            slackSend channel: 'alerts', baseUrl: 'https://nfq-international.slack.com', teamDomain: 'nfq-international' ,token: env.trainee_webhook_token, message: 'Hello World'
+            slackSend channel: 'alerts', baseUrl: 'https://nfq-international.slack.com', teamDomain: 'nfq-international' ,token: environment.trainee_webhook_token, message: 'Hello World'
         }
     } 
 }
